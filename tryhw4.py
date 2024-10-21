617f1a318571428af4ca858af5458a8707f9d9a7
rkc8626 <82958849+rkc8626@users.noreply.github.com>
2024-10-21 01:04:08 -0400

    Add files via upload

---
+++ tryhw4.py
@@ -0,0 +1,37 @@
+import sys
+sys.dont_write_bytecode = True
+from uwimg import *
+
+def softmax_model(inputs, outputs):
+    l = [make_layer(inputs, outputs, LRELU)]
+    return make_model(l)
+
+def neural_net(inputs, outputs):
+    print (inputs)
+    l = [   make_layer(inputs, 64, RELU),
+    		make_layer(64, 32, LRELU),
+            make_layer(32, outputs, SOFTMAX),]
+    return make_model(l)
+
+print("loading data...")
+train = load_classification_data(b"cifar.train", b"cifar/labels.txt", 1)
+test  = load_classification_data(b"cifar.test", b"cifar/labels.txt", 1)
+print("done")
+print
+
+print("training model...")
+batch = 128
+iters = 1000
+rate = .01
+momentum = .9
+decay = .001
+
+m = neural_net(train.X.cols, train.y.cols)
+train_model(m, train, batch, iters, rate, momentum, decay)
+print("done")
+print
+
+print("evaluating model...")
+print("training accuracy: %0.2f %%"%(100*accuracy_model(m, train)))
+print("test accuracy:     %0.2f %%"%(100*accuracy_model(m, test)))
+
